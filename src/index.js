0cef4cd01cfdd7f71eb6ef9371422e6efc3697f3
zabhitak <zabhitak1292@gmail.com>
2020-05-24 18:12:13 +0530

    state_react

--- src/index.js
+++ src/index.js
@@ -1,50 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
 // const App = () =>{
 //     window.navigator.geolocation.getCurrentPosition(
 //         (position) => console.log(position),
-//         (err)=> console.log('abhinav') 
+//         (err)=> console.log('abhinav')
 //     )
 //     return <div>  </div>
 // }
 
-class App extends React.Component{
-    constructor(props){
-        super(props); 
-        
-        this.state = { lat: null, errorMessage: ''}; //state object
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude })
-                //we did not
-                //this.state.lat = position.coord.latitiude
-            },
-            (err)=> {
-                this.setState({errorMessage: err.message });
-            }  
-        );
+class App extends React.Component {
+  // constructor(props){
+  //     super(props);
+
+  //     this.state = { lat: null, errorMessage: ''}; //state object
+  //     window.navigator.geolocation.getCurrentPosition(
+  //         (position) => {
+  // this.setState({ lat: position.coords.latitude })
+  //we did not
+  //this.state.lat = position.coord.latitiude
+  // },
+  // (err)=> {
+  //     this.setState({errorMessage: err.message });
+  // }
+  // );
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
-    render() {
-        if(this.state.errorMessage && !this.state.lat){
-        return <div>Error: {this.state.errorMessage}</div>
-        }
-
-        if(!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
-        }
-        
-            return <div>Loading</div>
-
-        // return (
-        // <div>
-        //     Latitude :{ this.state.lat } 
-        //     <br /> 
-        //     Error: {this.state.errorMessage}
-        // </div>
-        // )
+    if (!this.state.errorMessage && this.state.lat) {
+    //   return <div>Latitude: {this.state.lat}</div>;
+    return <SeasonDisplay 
+    lat= {this.state.lat}/>
     }
+
+    return <Spinner message="Please accept location request" />;
+  }
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
