ff0912d5859e7c2e543d2a2996eed0b53e9de04e
zabhitak <zabhitak1292@gmail.com>
2020-05-24 14:05:20 +0530

    state

--- src/index.js
+++ src/index.js
@@ -13,21 +13,37 @@ class App extends React.Component{
     constructor(props){
         super(props); 
         
-        this.state = { lat: 40 }; //state object
+        this.state = { lat: null, errorMessage: ''}; //state object
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ lat: position.coords.latitude })
                 //we did not
                 //this.state.lat = position.coord.latitiude
             },
-            (err)=> console.log('abhinav')  
+            (err)=> {
+                this.setState({errorMessage: err.message });
+            }  
         );
     }
 
     render() {
+        if(this.state.errorMessage && !this.state.lat){
+        return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>
+        }
         
-        
-        return <div>Latitude :{ this.state.lat } </div>
+            return <div>Loading</div>
+
+        // return (
+        // <div>
+        //     Latitude :{ this.state.lat } 
+        //     <br /> 
+        //     Error: {this.state.errorMessage}
+        // </div>
+        // )
     }
 }
 
